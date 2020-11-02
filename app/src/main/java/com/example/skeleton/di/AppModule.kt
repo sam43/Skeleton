1b2699676ecefda2b8cd77b19164e2f908c73edc
Saadat Sayem <sayem.ruet103043@gmail.com>
2020-11-02 23:37:44 +0600

    updated the retrofit call with interceptor and coroutine adapter

--- app/src/main/java/com/example/skeleton/di/AppModule.kt
+++ app/src/main/java/com/example/skeleton/di/AppModule.kt
@@ -8,11 +8,15 @@ import com.example.skeleton.data.remote.CharacterService
 import com.example.skeleton.data.repository.CharacterRepository
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -23,10 +27,12 @@ object AppModule {
 
     @Singleton
     @Provides
-    fun provideRetrofit(gson: Gson) : Retrofit = Retrofit.Builder()
-        .baseUrl("https://rickandmortyapi.com/api/")
-        .addConverterFactory(GsonConverterFactory.create(gson))
-        .build()
+    fun provideRetrofit(gson: Gson): Retrofit = Retrofit.Builder()
+            .client(client)
+            .baseUrl("https://rickandmortyapi.com/api/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .build()
 
     @Provides
     fun provideGson(): Gson = GsonBuilder().create()
@@ -50,5 +56,29 @@ object AppModule {
     @Provides
     fun provideRepository(remoteDataSource: CharacterRemoteDataSource,
                           localDataSource: CharacterDao) =
-        CharacterRepository(remoteDataSource, localDataSource)
+            CharacterRepository(remoteDataSource, localDataSource)
+
+    //Creating Auth Interceptor to add api_key query in front of all the requests.
+    private val authInterceptor = Interceptor { chain ->
+        val newUrl = chain.request().url
+                .newBuilder()
+                .build()
+
+        val newRequest = chain.request()
+                .newBuilder()
+                .url(newUrl)
+                .build()
+
+        chain.proceed(newRequest)
+    }
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient().newBuilder()
+            .addInterceptor(authInterceptor)
+            .addInterceptor(loggingInterceptor)
+            .build()
+
 }
