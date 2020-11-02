02fa23ac0dcdf13d89c3ca2e46859d62dadd89dc
Saadat Sayem <sayem.ruet103043@gmail.com>
2020-11-02 23:41:35 +0600

    updated documentation

--- app/src/main/java/com/example/skeleton/utils/ViewModelFactory.kt
+++ app/src/main/java/com/example/skeleton/utils/ViewModelFactory.kt
@@ -3,6 +3,11 @@ package com.example.skeleton.utils
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
+/**
+ * Coroutines and ktx already providing a factory to initialize a viewmodel lazy,
+ * No need of this custom viewmodel factory class anymore
+ * */
+
 class ViewModelFactory<T>(val creator: () -> T) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return creator() as T
